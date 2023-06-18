a45f96777cb532b5ac1249ed2883f8e82b25de6e
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-06-18 05:07:07 -0300

    03 - ADDED module content-service

---
+++ src/test/java/dev/ednascimento/contentservice/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ednascimento.contentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
