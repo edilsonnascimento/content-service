b8b1a5e11e122215d1469d3fb12f82c23c4e5964
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-06-19 06:50:21 -0300

    11 - FIXED injection

--- src/main/java/dev/ednascimento/contentservice/config/ClientConfig.java
+++ src/main/java/dev/ednascimento/contentservice/config/ClientConfig.java
@@ -2,10 +2,12 @@ package dev.ednascimento.contentservice.config;
 
 import dev.ednascimento.contentservice.service.ArticleClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+@Configuration
 public class ClientConfig {
 
     @Bean
